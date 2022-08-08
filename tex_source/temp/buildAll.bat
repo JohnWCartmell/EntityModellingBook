150d8070d78667a8e5f5172e1ca3cdb1fc6f01bc
JohnWCartmell <john.w.cartmell@gmail.com>
2022-08-08 12:36:16 +0100

    Moved into GitHub from BitBucket.

---
+++ tex_source/temp/buildAll.bat
@@ -0,0 +1,72 @@
+latex mainChenTransform.tex
+dvips -P pdf "mainChenTransform.dvi"
+ps2pdf mainChenTransform.ps
+convert -density 1000x1000 mainChenTransform.pdf -quality 95 ..\..\testwebsite\images\\ChenTransform.png
+latex mainbenzene.tex
+dvips -P pdf "mainbenzene.dvi"
+ps2pdf mainbenzene.ps
+convert -density 1000x1000 mainbenzene.pdf -quality 95 ..\..\testwebsite\images\\benzene.png
+latex maincomplextransitive.tex
+dvips -P pdf "maincomplextransitive.dvi"
+ps2pdf maincomplextransitive.ps
+convert -density 1000x1000 maincomplextransitive.pdf -quality 95 ..\..\testwebsite\images\\complextransitive.png
+latex maincopulative.tex
+dvips -P pdf "maincopulative.dvi"
+ps2pdf maincopulative.ps
+convert -density 1000x1000 maincopulative.pdf -quality 95 ..\..\testwebsite\images\\copulative.png
+latex maindiprepositional.tex
+dvips -P pdf "maindiprepositional.dvi"
+ps2pdf maindiprepositional.ps
+convert -density 1000x1000 maindiprepositional.pdf -quality 95 ..\..\testwebsite\images\\diprepositional.png
+latex mainditransitive.tex
+dvips -P pdf "mainditransitive.dvi"
+ps2pdf mainditransitive.ps
+convert -density 1000x1000 mainditransitive.pdf -quality 95 ..\..\testwebsite\images\\ditransitive.png
+latex mainethylene.tex
+dvips -P pdf "mainethylene.dvi"
+ps2pdf mainethylene.ps
+convert -density 1000x1000 mainethylene.pdf -quality 95 ..\..\testwebsite\images\\ethylene.png
+latex mainfowlersentenceexample.tex
+dvips -P pdf "mainfowlersentenceexample.dvi"
+ps2pdf mainfowlersentenceexample.ps
+convert -density 1000x1000 mainfowlersentenceexample.pdf -quality 95 ..\..\testwebsite\images\\fowlersentenceexample.png
+latex mainintransitive.tex
+dvips -P pdf "mainintransitive.dvi"
+ps2pdf mainintransitive.ps
+convert -density 1000x1000 mainintransitive.pdf -quality 95 ..\..\testwebsite\images\\intransitive.png
+latex mainlovegraph.tex
+dvips -P pdf "mainlovegraph.dvi"
+ps2pdf mainlovegraph.ps
+convert -density 1000x1000 mainlovegraph.pdf -quality 95 ..\..\testwebsite\images\\lovegraph.png
+latex mainmonoprepositional.tex
+dvips -P pdf "mainmonoprepositional.dvi"
+ps2pdf mainmonoprepositional.ps
+convert -density 1000x1000 mainmonoprepositional.pdf -quality 95 ..\..\testwebsite\images\\monoprepositional.png
+latex mainmonotransitive.tex
+dvips -P pdf "mainmonotransitive.dvi"
+ps2pdf mainmonotransitive.ps
+convert -density 1000x1000 mainmonotransitive.pdf -quality 95 ..\..\testwebsite\images\\monotransitive.png
+latex mainnounphrase.tex
+dvips -P pdf "mainnounphrase.dvi"
+ps2pdf mainnounphrase.ps
+convert -density 1000x1000 mainnounphrase.pdf -quality 95 ..\..\testwebsite\images\\nounphrase.png
+latex mainpath.tex
+dvips -P pdf "mainpath.dvi"
+ps2pdf mainpath.ps
+convert -density 1000x1000 mainpath.pdf -quality 95 ..\..\testwebsite\images\\path.png
+latex mainsubjectpredicate.tex
+dvips -P pdf "mainsubjectpredicate.dvi"
+ps2pdf mainsubjectpredicate.ps
+convert -density 1000x1000 mainsubjectpredicate.pdf -quality 95 ..\..\testwebsite\images\\subjectpredicate.png
+latex maintheatheletesetanewrecord.tex
+dvips -P pdf "maintheatheletesetanewrecord.dvi"
+ps2pdf maintheatheletesetanewrecord.ps
+convert -density 1000x1000 maintheatheletesetanewrecord.pdf -quality 95 ..\..\testwebsite\images\\theatheletesetanewrecord.png
+latex mainthewildyyappingdogonthesofa.tex
+dvips -P pdf "mainthewildyyappingdogonthesofa.dvi"
+ps2pdf mainthewildyyappingdogonthesofa.ps
+convert -density 1000x1000 mainthewildyyappingdogonthesofa.pdf -quality 95 ..\..\testwebsite\images\\thewildyyappingdogonthesofa.png
+latex mainwater.tex
+dvips -P pdf "mainwater.dvi"
+ps2pdf mainwater.ps
+convert -density 1000x1000 mainwater.pdf -quality 95 ..\..\testwebsite\images\\water.png
